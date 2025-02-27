9feb0d1f38240cfa9f68daf9972e364eb3e2d7a8
btbrad <biantaobrad@gmail.com>
2025-02-27 09:57:22 +0800

    原始类型和对象类型

--- src/beginner/01.ts
+++ src/beginner/01.ts
@@ -1,2 +1,13 @@
 const msg: string = 'Hello World !'
 console.log(msg)
+
+let userAge: number = 0
+// userAge = "test" // error
+
+const set = new Set<number>()
+set.add(1)
+// set.add('2') // error
+
+const map = new Map<number, string>()
+map.set(1, '1')
+// map.set('2', '2') // error
